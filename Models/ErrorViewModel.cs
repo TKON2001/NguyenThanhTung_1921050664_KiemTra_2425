3170f4d4b4cac557f5ff14d426697bcc91e0c58e
NguyenThanhTung_1921050664_KiemTra_2425 <tkon33322@gmail.com>
2025-05-23 18:47:38 +0700

    BaiKT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThanhTung_1921050664_KiemTra.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
